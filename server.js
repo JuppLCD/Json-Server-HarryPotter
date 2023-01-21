fc4b493e4a61fdabe5f87ac6e4934212aedbcea0
JuppLCD <lorenzocanovas207@gmail.com>
2023-01-20 21:59:26 -0300

    Add cors

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db/db.json');
-const middlewares = jsonServer.defaults();
+// const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
+const corsOptions = {
+	origin: 'https://harry-potter-react-ts.netlify.app',
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+server.use(cors(corsOptions));
+// server.use(middlewares);
 server.use(router);
 
 server.listen(port);
